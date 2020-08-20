a310fc638560403d7af927b65e070ec9a43da337
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-08-21 03:49:52 +0900

    出品情報保存機能の実装（画像以外）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @tweets = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :information, :category, :status, :delivery_fee, :delivery_area, :delivery_days, :price, :user_id)
   end
 end
 
-def create
-  @item.save!
-  
-end
+# def create
+#   @item.save!
+# end
