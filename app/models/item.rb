4b0e87e0d4acec55f34509e1d18a42ffb0c485de
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-08-21 17:12:44 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :information
+    validates :category
+    validates :status
+    validates :delivery_fee
+    validates :delivery_area
+    validates :delivery_days
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    end
 end
