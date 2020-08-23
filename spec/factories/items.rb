9fe476ec97cae51ad885fc73d412432970ada8d4
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-08-23 16:03:18 +0900

    テストコードの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name          {"商品aA1１"}
+    information   {"この商品はアメリカ(Usa)産の3３"}
+    category      {1}
+    status        {1}
+    delivery_fee  {1}
+    delivery_area {1}
+    delivery_days {1}
+    price         {"300"}
   end
 end
