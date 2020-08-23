30e3d6ea819d274c00c03ac700d3745bfe37b3a0
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-08-23 19:03:16 +0900

    正常系テストコードを実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name          {"商品aA1１"}
-    information   {"この商品はアメリカ(Usa)産の3３"}
-    category      {1}
-    status        {1}
-    delivery_fee  {1}
-    delivery_area {1}
-    delivery_days {1}
+    name          {"商品名"}
+    information   {"商品の説明"}
+    category      {"1"}
+    status        {"1"}
+    delivery_fee  {"1"}
+    delivery_area {"1"}
+    delivery_days {"1"}
     price         {"300"}
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  context '商品出品機能' do
+  context '商品出品機能の正常系テストコード' do
+    it "出品に必要な情報が入力されていれば情報が登録される" do
+    expect(@item).to be_valid
+    end
+  end
+
+  context '商品出品機能の異常系テストコード' do
     it "商品名が空だと登録できない" do
       @item.name = ""
       @item.valid?
