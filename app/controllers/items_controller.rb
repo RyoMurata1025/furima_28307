86496375f29773344ae9863bab03de69aa152cb9
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-08-26 15:42:59 +0900

    destroyアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -7,6 +7,11 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def update
