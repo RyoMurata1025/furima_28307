7bc05d4c8bf76f9a8f48e76e9f65bed0e0ed6632
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-09-02 11:02:13 +0900

    「画像が0枚の場合でも出品できてしまう」の解決

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,7 @@ class Item < ApplicationRecord
   has_one :buyer
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :information
     validates :category_id
@@ -20,4 +21,5 @@ class Item < ApplicationRecord
     validates :delivery_days_id
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
     end
+
 end
